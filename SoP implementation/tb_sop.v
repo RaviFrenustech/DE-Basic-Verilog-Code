8e212e0aedefde98fc6280f5822fddd6d42f0c51
Ravi Kumar Pathak <ravi.pathak@frenustech.com>
2022-03-23 10:29:26 +0530

    p commit

--- SoP implementation/tb_sop.v
+++ SoP implementation/tb_sop.v
@@ -12,8 +12,8 @@ begin: apply_stimulus
             {i1,i2,i3,i4} = v [4:0];
             #10 $display("ABCD = %b, Out(Z) = %b ",{i1, i2, i3, i4},o1);
         end
-$dumpfile ("tb_sop.v");
-$dumpfile(0, tb_sop);
+$dumpfile ("tb_sop.vcd");
+$dumpvars(0, tb_sop);
 end
 sop int1 (.i1(i1), .i2(i2), .i3(i3), .i4(i4), .o1(o1));
 endmodule
