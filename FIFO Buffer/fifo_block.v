0c0ee918af4fa82bcf52310d2588ba56594d9023
Ravi Kumar Pathak <ravi.pathak@frenustech.com>
2022-03-23 13:36:05 +0530

    last commit

--- FIFO Buffer/fifo_block.v
+++ FIFO Buffer/fifo_block.v
@@ -1,46 +1,62 @@
-module fifo1 (input [7:0] data_in, input clk, rst, rd, wr, output empty, full, output reg [7:0] data_out, output reg [3:0] fifo_cnt);
-    reg [7:0] fifo_ram[0:7];
-    reg [2:0] read_ptr, wr_ptr;
-
-    assign empty = (fifo_cnt==0);
-    assign full = (fifo_cnt==8);
-
-    always @( posedge clk) 
-    begin: write
-        if(wr && !full)
-            fifo_ram[wr_ptr] <= data_in;
-        else if (wr && full)
-            fifo_ram[wr_ptr] <= data_in;    
-    end
-    always @ (posedge clk)
-    begin: read
-        if(rd && !empty)
-            data_out <= fifo_ram[read_ptr];
-        else if (rd && wr && empty)
-            data_out <= fifo_ram[read_ptr];
-    end
-    always @ (posedge clk)
-    begin: pointer
-    if (rst) begin
-        wr_ptr <= 0;
-        read_ptr <= 0;
-    end
-    else begin
-            wr_ptr <= ((wr && !full)||(wr && rd)) ? wr_ptr + 1 : wr_ptr;
-            read_ptr <= ((rd && !empty)||(wr && rd)) ? read_ptr + 1 : read_ptr;
+`timescale 1ns/1ps
+module fifo(clk,reset,wr_en,rd_en,din,dout,full,empty);
+  input clk,reset;
+  input wr_en,rd_en;
+  input [7:0]din;
+  output [7:0]dout;
+  output full,empty;
+  
+  reg [7:0]mem[0:15];//declaration of RAM
+  wire clk,reset;
+  wire wr_en,rd_en;
+  wire [7:0]din;
+  reg [7:0]dout;
+  wire full,empty;
+  reg [4:0]addr;
+  integer i;
+  assign full = (addr ==5'b10000) ? 1'b1 : 1'b0;
+  assign empty = (addr ==5'b00000) ? 1'b1 : 1'b0;
+  
+  always@(posedge clk)
+    begin
+      if(reset)
+        begin
+        	addr = 4'b0000;
+      		for(i=0;i<=15;i=i+1) 
+        		mem[i] = 8'b0;
         end
-     end
-    always @ (posedge clk)
-    begin:Count
-    if(rst) fifo_cnt <=0;
-    else begin
-            case ({wr,rd})
-               2'b00 : fifo_cnt <= fifo_cnt;
-               2'b01 : fifo_cnt <= (fifo_cnt==0) ? 0 : fifo_cnt-1;
-               2'b10 : fifo_cnt <= (fifo_cnt==8) ? 0 : fifo_cnt+1;
-               2'b11 : fifo_cnt <= fifo_cnt;
-               default: fifo_cnt <= fifo_cnt; 
-            endcase
+      else if(wr_en | rd_en)//read or write
+        begin
+          if(wr_en && (!full))
+            begin
+            	mem[addr]=din;
+            	addr = addr + 1;
+            end
+          else
+            if(rd_en && (!empty))
+              begin
+                dout =mem[0];
+                mem[0] = mem[1];
+                mem[1] = mem[2];
+                mem[2] = mem[3];
+                mem[3] = mem[4];
+                mem[4] = mem[5];
+                mem[5] = mem[6];
+                mem[6] = mem[7];
+                mem[7] = mem[8];
+                mem[8] = mem[9];
+                mem[9] = mem[10];
+                mem[10] = mem[11];
+                mem[11] = mem[12];
+                mem[12] = mem[13];
+                mem[13] = mem[14];
+                mem[14] = mem[15];
+                mem[15] = 8'b0;
+                   addr = addr - 1;         
+                
+              end
+              
         end
+      
     end
 endmodule
